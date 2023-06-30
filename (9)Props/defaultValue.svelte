075dede149deb5d6cf20b83809f66d591aadeec0
ksb96 <52975077+ksb96@users.noreply.github.com>
2023-06-30 15:17:35 +0530

    props added

---
+++ (9)Props/defaultValue.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Nested from "./Nested.svelte";
+</script>
+
+<!-- nested-correspond to the file name -->
+<Nested answer={42} />
+
+<!-- The answer is Hi -->
+<Nested />
---
+++ (9)Props/nested.svelte
@@ -0,0 +1,6 @@
+<script>
+    // default value
+    export let answer = 'Hi';
+</script>
+
+<p>The answer is {answer}</p>
